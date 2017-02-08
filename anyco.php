47f255cb5a6f0562263fbd0ec4930d2e0a1a9c49
mhaculetz23 <vtcabana@gmail.com>
2017-02-08 12:42:11 +0800

    Update anyco.php

--- anyco.php
+++ anyco.php
@@ -1,7 +1,29 @@
-<?php
+$conn = oci_connect(username,password,server);
+username = 'hr'
+password = 'hr'
+server = '//localhost/XE'
 
+<?php
 require('anyco_ui.inc');
-ui_print_header('Department');
+
+$conn = oci_connect('hr','hr','//localhost/XE')
+ui_print_header('Departments');
+do_query($conn,'SELECT * FROM DEPARTMENTS');
 ui_print_footer(date('Y-m-d H:i:s'));
 
+function do_query($conn,$query){
+  $stid = oci_parse($conn,$query);
+  $r = oci_execute($stid,OCI_DEFAULT);
+  print '<table border="1">';
+  
+  while ($row=oci_fetch_array($stid,OCI_RETURN_NULLS)){
+    print'<tr>';
+    foreach($row as $item){
+      print '<td>'.
+        ($item ?htmlentities($item): '&nbsp;' . '</td>';
+         }
+         print '</tr>';
+         }
+         print '</table>';
+         }
 ?>
